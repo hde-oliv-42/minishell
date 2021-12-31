8a326ab28d20017a2fd645ff666d192049a2e250
Paulo Santana <paulo.santana.r@gmail.com>
2021-12-30 21:15:51 -0300

    chore: add example main for fast testing

--- Makefile
+++ Makefile
@@ -1,15 +1,26 @@
 NAME = minishell
 
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+INCLUDE = -I src -I $(LIBFT_DIR)
+HEADERS = builtins.h parsing.h
+
 RM = rm -rf
 CC = gcc
-CFLAGS = -Wall -Werror -Wextra -g
-LFLAGS = -lreadline
+CFLAGS = -Wall -Werror -Wextra -g $(INCLUDE)
+LFLAGS = -lreadline -L $(LIBFT_DIR) -lft
 
-INCLUDE = include
+vpath %.c src src/builtins src/process src/parsing
 
-vpath %.c src src/builtins
+TEST_SRC = pwd.c 				\
+		   process.c			\
+		   pipeline.c			\
+		   destroy.c			\
+		   utils.c				\
+		   redirection.c		\
+		   string.c
 
-TEST_SRC = pwd.c
 SRC = main.c $(TEST_SRC)
 
 TEST_FILES = ./tests/buitins/pwd.test.c 
@@ -21,16 +32,19 @@ TEST_OBJ = $(addprefix $(OBJ_DIR)/, $(TEST_SRC:.c=.o))
 
 all: $(NAME)
 
-$(NAME): $(OBJ_DIR) $(OBJ)
-	$(CC) $(LFLAGS) -o $(NAME) $(OBJ)
+$(NAME): $(LIBFT) $(OBJ_DIR) $(OBJ)
+	$(CC) -o $(NAME) $(OBJ) $(LFLAGS)
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
 
 $(OBJ_DIR):
 	mkdir -p $(OBJ_DIR)
 
-$(OBJ_DIR)/%.o: %.c $(HEADERS)
+$(OBJ_DIR)/%.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-run:
+run: $(NAME)
 	./$(NAME)
 
 clean:
@@ -38,6 +52,9 @@ clean:
 
 fclean: clean
 	$(RM) $(NAME)
+	echo $(SRC)
+	echo $(OBJ)
+	make -C $(LIBFT_DIR) fclean
 
 re: fclean all
 
--- src/main.c
+++ src/main.c
@@ -2,11 +2,117 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 
+#include <process/process.h>
+#include <parsing/parsing.h>
+#include <libft.h>
+
+void	print_pipeline(t_program *program)
+{
+	t_list	*params;
+	t_list	*infile;
+	t_list	*outfile;
+	t_redirection *redirection;
+
+	while (program)
+	{
+		printf("=== program ===\n");
+		printf("  name: %s\n", program->name);
+		printf("  params: [ ");
+		params = program->params;
+		while (params)
+		{
+			printf("%s, ", (char *)params->content);
+			params = params->next;
+		}
+		printf("NULL ]\n");
+		printf("  infiles: [ ");
+		infile = program->input_list;
+		while (infile)
+		{
+			redirection = infile->content;
+			printf("<");
+			if (redirection->type == HERE_DOC)
+				printf("<");
+			printf(" %s, ", redirection->file_name);
+			infile = infile->next;
+		}
+		printf("NULL ]\n");
+		printf("  output: [ ");
+		outfile = program->output_list;
+		while (outfile)
+		{
+			redirection = outfile->content;
+			printf(">");
+			if (redirection->type == OUTFILE_APPEND)
+				printf(">");
+			printf(" %s, ", redirection->file_name);
+			outfile = outfile->next;
+		}
+		printf("NULL ]\n");
+		if (program->next)
+		{
+			char *type;
+			if (program->next_relation == OR)
+				type = "||";
+			else if (program->next_relation == AND)
+				type = "&&";
+			else
+				type = "|";
+			printf("  next: %s %s\n", type, program->next->name);
+		}
+		program = program->next;
+	}
+}
+
+int	quit_minishell(void)
+{
+	// TODO: free stuff
+	return (0);
+}
+
+// TODO: validate the command string
+int	is_valid(char *line)
+{
+	(void)line;
+	// printf("minishell: parse error\n");
+	return (1);
+}
+
+char	*generate_prompt(void)
+{
+	return ("minishell$ ");
+}
+
+void	execute_pipeline(t_program *pipeline)
+{
+	while (pipeline)
+	{
+		// TODO: connect and execute the programs in the pipeline
+		pipeline = pipeline->next;
+	}
+}
+
 int	main(void)
 {
-	char	*line;
+	char		*line;
+	t_program	*pipeline;
+	int			cursor;
+	char		*prompt;
 
-	line = readline("minishell > ");
-	printf("you typed: [ %s ]\n", line);
-	free(line);
+	while (1)
+	{
+		prompt = generate_prompt();
+		line = readline(prompt);
+		if (line == NULL)
+			return (quit_minishell());
+		if (!is_valid(line))
+			continue ;
+		cursor = 0;
+		pipeline = parse_pipeline(line, &cursor);
+		if (pipeline == NULL)
+			continue ;
+		print_pipeline(pipeline);
+		destroy_pipeline(pipeline);
+		free(line);
+	}
 }

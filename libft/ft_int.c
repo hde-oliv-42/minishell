42d2b28b7366dc7a1feb3026ddb185d1b1fabc97
hde-oliv <henriquerocha@tutanota.com>
2022-01-29 17:03:39 -0300

    fix: ft_int not compiling under gcc

--- libft/ft_int.c
+++ libft/ft_int.c
@@ -58,25 +58,17 @@ static size_t	ft_uintlen(unsigned int i)
 
 static char	*ft_uitoa(unsigned int n)
 {
-	size_t		i;
-	size_t		minus;
+	ssize_t		i;
 	char		*p;
 
 	i = ft_uintlen(n);
-	minus = n < 0;
-	i += minus;
 	p = (char *) malloc(sizeof(char) * i + 1);
 	if (p == NULL)
 		return (NULL);
 	p[i] = '\0';
-	if (minus)
-		p[0] = '-';
-	while (--i != (minus - 1))
+	while (--i != - 1)
 	{
-		if ((n % 10) < 0)
-			p[i] = ((n % 10) * -1) + 48;
-		else
-			p[i] = (n % 10) + 48;
+		p[i] = (n % 10) + 48;
 		n = n / 10;
 	}
 	return (p);

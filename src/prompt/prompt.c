717893abe30188b4fc5be0b7c85a9b7cf0de7f67
Paulo Santana <paulo.santana.r@gmail.com>
2022-01-30 22:06:04 -0300

    feat: show user and cwd in the prompt

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -23,13 +23,42 @@
 #define CYAN	"\001\x1b[36m\002"
 #define RESET	"\001\x1b[0m\002"
 
+static char	*get_last_three_dirs(void)
+{
+	int		i;
+	int		slashs;
+	char	*str;
+	char	*cwd;
+
+	slashs = 0;
+	cwd = getcwd(NULL, 0);
+	i = ft_strlen(cwd);
+	while (slashs < 3 && i > 0)
+	{
+		i--;
+		if (cwd[i] == '/')
+			slashs++;
+	}
+	if (i > 0)
+		i++;
+	str = ft_substr(cwd, i, ft_strlen(cwd) - i);
+	free(cwd);
+	return (str);
+}
+
 char	*generate_prompt(void)
 {
-	char	*cwd;
+	char	*pasta;
+	int		result;
 	char	*prompt;
+	char	*user;
 
-	cwd = getcwd(NULL, 0);
-	ft_asprintf(&prompt, BOLD BLUE "%s" GREEN " >>> " RESET, cwd);
-	free(cwd);
+	pasta = get_last_three_dirs();
+	user = getenv("USER");
+	result = ft_asprintf(&prompt, YELLOW "%s" BOLD BLUE " %s"
+			GREEN " >>> " RESET, user, pasta);
+	if (result == -1)
+		return (ft_strdup("[error generating prompt] > "));
+	free(pasta);
 	return (prompt);
 }

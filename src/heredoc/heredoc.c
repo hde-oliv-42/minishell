209afc4dfec8688451590d4b958ad4c75de6c060
Paulo Santana <paulo.santana.r@gmail.com>
2022-02-19 15:11:31 -0300

    refactor: move heredoc subprocess functions to related file

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -6,70 +6,19 @@
 /*   By: psergio- <psergio->                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 19:52:00 by psergio-          #+#    #+#             */
-/*   Updated: 2022/02/18 20:00:40 by psergio-         ###   ########.fr       */
+/*   Updated: 2022/02/19 15:09:59 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute/execute.h"
 #include "libft.h"
 #include <stdio.h>
-#include <readline/readline.h>
 #include <stdlib.h>
 #include "ft_printf/libftprintf.h"
 #include "heredoc.h"
 #include <minishell.h>
 #include "parsing/parsing.h"
 #include "signals/signals.h"
-
-static char	*finish_heredoc(t_list *lines)
-{
-	char	*full_text;
-	char	*tmp;
-
-	tmp = lines->content;
-	full_text = ft_strjoin(tmp, "\n");
-	lines = lines->next;
-	while (lines && lines->next)
-	{
-		tmp = full_text;
-		full_text = ft_strjoin(full_text, lines->content);
-		free(tmp);
-		tmp = full_text;
-		full_text = ft_strjoin(full_text, "\n");
-		free(tmp);
-		lines = lines->next;
-	}
-	return (full_text);
-}
-
-char	*get_heredoc(char *delimiter)
-{
-	char	*newstr;
-	char	*full_text;
-	t_list	*lines_list;
-
-	lines_list = NULL;
-	full_text = NULL;
-	while (1)
-	{
-		newstr = readline("> ");
-		if (newstr == NULL)
-		{
-			ft_dprintf(2, "unexpected end-of-file, expeceted '%s'\n",
-				delimiter);
-			ft_lstclear(&lines_list, free);
-			return (NULL);
-		}
-		ft_lstadd_back(&lines_list, ft_lstnew(newstr));
-		if (str_equals(delimiter, newstr))
-		{
-			full_text = finish_heredoc(lines_list);
-			ft_lstclear(&lines_list, free);
-			break ;
-		}
-	}
-	return (full_text);
-}
 
 static int	parent_receive_heredoc(
 	t_redirection *redirection, int piper[2])
--- src/heredoc/heredoc_subprocess.c
+++ src/heredoc/heredoc_subprocess.c
@@ -6,7 +6,7 @@
 /*   By: psergio- <psergio->                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:38:19 by psergio-          #+#    #+#             */
-/*   Updated: 2022/02/19 14:59:19 by psergio-         ###   ########.fr       */
+/*   Updated: 2022/02/19 15:09:59 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <libft.h>
 #include <minishell.h>
+#include <readline/readline.h>
 #include "heredoc.h"
 
 int	wait_child(void)
@@ -37,6 +38,56 @@ int	wait_child(void)
 		if (WEXITSTATUS(wstatus) == 0)
 			return (1);
 	return (0);
+}
+
+static char	*finish_heredoc(t_list *lines)
+{
+	char	*full_text;
+	char	*tmp;
+
+	tmp = lines->content;
+	full_text = ft_strjoin(tmp, "\n");
+	lines = lines->next;
+	while (lines && lines->next)
+	{
+		tmp = full_text;
+		full_text = ft_strjoin(full_text, lines->content);
+		free(tmp);
+		tmp = full_text;
+		full_text = ft_strjoin(full_text, "\n");
+		free(tmp);
+		lines = lines->next;
+	}
+	return (full_text);
+}
+
+char	*get_heredoc(char *delimiter)
+{
+	char	*newstr;
+	char	*full_text;
+	t_list	*lines_list;
+
+	lines_list = NULL;
+	full_text = NULL;
+	while (1)
+	{
+		newstr = readline("> ");
+		if (newstr == NULL)
+		{
+			ft_dprintf(2, "unexpected end-of-file, expeceted '%s'\n",
+				delimiter);
+			ft_lstclear(&lines_list, free);
+			return (NULL);
+		}
+		ft_lstadd_back(&lines_list, ft_lstnew(newstr));
+		if (str_equals(delimiter, newstr))
+		{
+			full_text = finish_heredoc(lines_list);
+			ft_lstclear(&lines_list, free);
+			break ;
+		}
+	}
+	return (full_text);
 }
 
 void	child_send_heredoc(
